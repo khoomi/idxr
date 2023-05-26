c218609fa523d9575609eafabeb36d9dac15c9ab
borngraced <samuelonoja970@gmail.com>
2023-05-26 22:22:58 +0100

    update user email
    Signed-off-by: borngraced <samuelonoja970@gmail.com>

--- email/user_email.go
+++ email/user_email.go
@@ -25,7 +25,7 @@ func SendWelcomeEmail(email, loginName string) {
 		ToName:     loginName,
 		Sender:     "no-reply@khoomi.com",
 		SenderName: "Khoomi Online",
-		Body:       fmt.Sprintf("<body>\n    <h1>Welcome to Khoomi - Connecting Nigerian Creatives to the World!</h1>\n    <p>Dear %v,</p>\n    <p>\n      We are thrilled to welcome you to Khoomi, the new e-commerce platform for\n      Nigerian creatives. Thank you for joining our community of talented\n      artisans, small business owners, and passionate shoppers.\n    </p>\n    <p>\n      At Khoomi, our mission is to connect Nigerian creatives with a wider\n      audience, helping you to showcase your products and services to a broader\n      market. Our platform offers a user-friendly interface that allows you to\n      easily list your products and reach customers across Nigeria and beyond.\n    </p>\n    <p>\n      As a new user, you can set up your seller account and start listing your\n      products in just a few simple steps. Our team is always here to help you if\n      you have any questions or concerns, so please don't hesitate to reach out.\n    </p>\n    <p>\n      We are excited to have you on board and can't wait to see the amazing\n      products and services you have to offer. Thank you for choosing Khoomi!\n    </p>\n    <p>Best,</p>\n    <p>The Khoomi Team</p>\n  </body>", loginName),
+		Body:       fmt.Sprintf("<html><head><style>body{font-family: Arial, sans-serif; font-size:14px;}p{margin-bottom: 10px;}.highlight{font-weight: bold;color: #FF5810;}</style></head><body><p>Dear <span class=\"highlight\">%v</span>,</p><p>We are thrilled to welcome you to Khoomi, the new e-commerce platform for Nigerian creatives. Thank you for joining our community of talented artisans, small business owners, and passionate shoppers.</p><p>At Khoomi, our mission is to connect Nigerian creatives with a wider audience, helping you to showcase your products and services to a broader market. Our platform offers a user-friendly interface that allows you to easily list your products and reach customers across Nigeria and beyond.</p><p>As a new user, you can set up your seller account and start listing your products in just a few simple steps. Our team is always here to help you if you have any questions or concerns, so please don't hesitate to reach out.</p><p>We are excited to have you on board and can't wait to see the amazing products and services you have to offer. Thank you for choosing Khoomi!</p><p>Best,</p><p>The Khoomi Team</p></body></html>", loginName),
 		Subject:    "Welcome to Khoomi - Connecting Nigerian Creatives to the World",
 	}
 
@@ -44,7 +44,7 @@ func SendVerifyEmailNotification(email, loginName, link string) {
 		ToName:     loginName,
 		Sender:     "no-reply@khoomi.com",
 		SenderName: "Khoomi Online",
-		Body:       fmt.Sprintf("<body>\n        <p>Dear %v,</p>\n        <p>Thank you for creating an account with us!</p>\n        <p>Please click the following link to verify your email address:</p>\n        <p><a href=\"%v\">%v</a></p>\n        <p>The link is valid for 24 hours.</p>\n        <p>Best regards,</p>\n        <p>Your Application Team</p>\n    </body>", loginName, link, link),
+		Body:       fmt.Sprintf(`<body style="font-family: Arial, sans-serif; font-size: 14px;"><p>Dear %v,</p><p>Thank you for creating an account with us!</p><p>Please click the following link to verify your email address:</p><p><a href="%v" style="color: #FF5810; text-decoration: none;">%v</a></p><p>The link is valid for 24 hours.</p><p>Best regards,</p><p>Your Application Team</p></body>`, loginName, link, link),
 		Subject:    "Verify your email on Khoomi",
 	}
 
@@ -62,8 +62,17 @@ func SendPasswordResetEmail(email, loginName, link string) {
 		ToName:     loginName,
 		Sender:     "no-reply@khoomi.com",
 		SenderName: "Khoomi Online",
-		Body:       fmt.Sprintf("<body>\n    <h1>Khoomi Password Reset Request</h1>\n    <p>Dear %v,</p>\n    <p>\n      We received a request to reset the password for your Khoomi account. To\n      reset your password, please click the button below:\n    </p>\n    <div style=\"text-align: center;\">\n      <a\n        href=\"%v\"\n        style=\"\n          background-color: #FF5810;\n          color: #ffffff;\n          border-radius: 30px;\n          display: inline-block;\n          font-size: 16px;\n          font-weight: bold;\n          padding: 10px 16px;\n          text-align: center;\n          text-decoration: none;\n        \"\n        >Reset Password</a\n      >\n    </div>\n    <p>\n      If you did not request a password reset, please ignore this message and\n      your password will remain unchanged.\n    </p>\n    <p>Thank you,</p>\n    <p>The Khoomi Team</p>\n  </body>", loginName, link),
-		Subject:    "Khoomi Password Reset Request",
+		Body: fmt.Sprintf(`<body style="font-family: Arial, sans-serif; font-size: 14px;">
+    <p>Dear %v,</p>
+    <p>We received a request to reset the password for your Khoomi account. To reset your password, please click the button below:</p>
+    <div style="text-align: center;">
+        <a href="%v" style="background-color: #FF5810; color: #ffffff; border-radius: 30px; display: inline-block; font-size: 16px; font-weight: bold; padding: 10px 16px; text-align: center; text-decoration: none;">Reset Password</a>
+    </div>
+    <p>If you did not request a password reset, please ignore this message and your password will remain unchanged.</p>
+    <p>Thank you,</p>
+    <p>The Khoomi Team</p>
+</body>`, loginName, link),
+		Subject: "Khoomi Password Reset Request",
 	}
 
 	err := services.SendMail(mail)
@@ -80,7 +89,7 @@ func SendPasswordResetSuccessfulEmail(email, loginName string) {
 		ToName:     loginName,
 		Sender:     "no-reply@khoomi.com",
 		SenderName: "Khoomi Online",
-		Body:       fmt.Sprintf("<body>\n        <p>Dear %v,</p>\n        <p>Your password has been successfully reset.</p>\n        <p>If you did not request this password reset, please contact us immediately.</p>\n        <p>Best regards,</p>\n        <p>Your Application Team</p>\n    </body>", loginName),
+		Body:       fmt.Sprintf("<body style=\"font-family: Arial, sans-serif; font-size: 14px;><p>Dear %v,</p><p>Your password has been successfully reset.</p><p>If you did not request this password reset, please contact us immediately.</p><p>Best regards,</p><p>Your Application Team</p></body>", loginName),
 		Subject:    "Password Reset Successful",
 	}
 
@@ -99,7 +108,7 @@ func SendNewIpLoginNotification(email, loginName, IP string, loginTime time.Time
 		ToName:     loginName,
 		Sender:     "no-reply@khoomi.com",
 		SenderName: "Khoomi Online",
-		Body:       fmt.Sprintf(" <body>\n        <p>Dear %v,</p>\n        <p>This is to inform you that a new IP address has been used to log in to your account at %v.</p>\n        <p>IP Address: %v</p>\n         <p>If you did not log in from this location, please contact us or change your password immediately.</p>\n        <p>Best regards,</p>\n        <p>Your Application Team</p>\n    </body>", loginName, loginTime, IP),
+		Body:       fmt.Sprintf(" <body style=\"font-family: Arial, sans-serif; font-size: 14px;><p>Dear %v,</p><p>This is to inform you that a new IP address has been used to log in to your account at %v.</p><p>IP Address: %v</p> <p>If you did not log in from this location, please contact us or change your password immediately.</p><p>Best regards,</p><p>Your Application Team</p></body>", loginName, loginTime, IP),
 		Subject:    "New IP Address Login Notification",
 	}
 	err := services.SendMail(mail)
