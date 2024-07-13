7642ee3158c67b41a4b9f2cd01460f197145f076
Samuel Onoja <samiodev@icloud.com>
2024-07-13 03:17:30 +0100

    minor changes

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -170,12 +170,12 @@ type RefreshTokenPayload struct {
 type Notification struct {
 	// ID of the shop.
 	ID               primitive.ObjectID `bson:"_id" json:"_id" validate:"required"`
-	UserID           primitive.ObjectID `bson:"user_id" json:"user_id" validate:"required"`
-	NewMessage       bool               `bson:"new_message" json:"new_message" validate:"required"`
-	NewFollower      bool               `bson:"new_follower" json:"new_follower" validate:"required"`
-	ListingExpNotice bool               `bson:"listing_exp_notice" json:"listing_exp_notice" validate:"required"`
-	SellerActivity   bool               `bson:"seller_activity" json:"seller_activity" validate:"required"`
-	NewsAndFeatures  bool               `bson:"news_and_features" json:"news_and_features" validate:"required"`
+	UserID           primitive.ObjectID `bson:"user_id" json:"userId" validate:"required"`
+	NewMessage       bool               `bson:"new_message" json:"newMessage" validate:"required"`
+	NewFollower      bool               `bson:"new_follower" json:"newFollower" validate:"required"`
+	ListingExpNotice bool               `bson:"listing_exp_notice" json:"listingExpNotice" validate:"required"`
+	SellerActivity   bool               `bson:"seller_activity" json:"sellerActivity" validate:"required"`
+	NewsAndFeatures  bool               `bson:"news_and_features" json:"newsAndFeatures" validate:"required"`
 }
 
 type NotificationRequest struct {
